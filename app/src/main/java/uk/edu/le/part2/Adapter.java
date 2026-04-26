81d79f1c7de03d228c4f012efbf2a5be8b5ef63d
joss-ham <josshamblen@gmail.com>
2026-04-26 18:45:50 +0100

    -added holder class and implementations to adapter class

--- app/src/main/java/uk/edu/le/part2/Adapter.java
+++ app/src/main/java/uk/edu/le/part2/Adapter.java
@@ -1,18 +1,18 @@
 package uk.edu.le.part2;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
+import uk.edu.le.part2.data.SafetyCheck;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.List;
 
 import uk.edu.le.part2.data.SafetyCheck;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder>{
-    //TODO make holder class
     private List<SafetyCheck> checkList;
 
     public Adapter(List<SafetyCheck> checkList) {
@@ -26,4 +26,29 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder>{
                 .inflate(R.layout.recycler_card, parent, false);
         return new MyViewHolder(view);
     }
+
+    @Override
+    public int getItemCount(){
+        return checkList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.MyViewHolder holder, int position) {
+        SafteyCheck safteyCheck = checkList.get(position);
+
+        holder.checkId.setText(SafetyCheck.getCheckId());
+        //add all columns later
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView id, date, vehicleRegistration, driverName, overallStatus; //pull info from defect later
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+                checkId = itemView.findViewById(R.id.checkId);
+                //add all columns later
+
+        }
+    }
 }
+
+
